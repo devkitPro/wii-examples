024a3d17066793b7996d68ec23ae40eb8c76eb08
Michael Wiedenbauer <shagkur@users.sourceforge.net>
2008-06-03 21:18:03 +0000

    fixed example

--- graphics/gx/gxSprites/source/gxsprites.c
+++ graphics/gx/gxSprites/source/gxsprites.c
@@ -17,7 +17,7 @@
 #define DEFAULT_FIFO_SIZE	(1024*1024)
 
 static void *frameBuffer[2] = { NULL, NULL};
-GXRModeObj *rmode;
+static GXRModeObj *rmode;
 
 #define NUM_SPRITES 1024
 
@@ -37,14 +37,14 @@ void drawSpriteTex( int x, int y, int width, int height, int image );
 //---------------------------------------------------------------------------------
 int main( int argc, char **argv ){
 //---------------------------------------------------------------------------------
+	u32	fb; 	// initial framebuffer index
+	u32 first_frame;
 	f32 yscale;
-
 	u32 xfbHeight;
-
 	Mtx44 perspective;
 	Mtx GXmodelView2D;
+	void *gp_fifo = NULL;
 
-	u32	fb = 0; 	// initial framebuffer index
 	GXColor background = {0, 0, 0, 0xff};
 
 	int i;
@@ -54,6 +54,8 @@ int main( int argc, char **argv ){
  
 	rmode = VIDEO_GetPreferredMode(NULL);
 	
+	fb = 0;
+	first_frame = 1;
 	// allocate 2 framebuffers for double buffering
 	frameBuffer[0] = MEM_K0_TO_K1(SYS_AllocateFramebuffer(rmode));
 	frameBuffer[1] = MEM_K0_TO_K1(SYS_AllocateFramebuffer(rmode));
@@ -65,8 +67,9 @@ int main( int argc, char **argv ){
 	VIDEO_WaitVSync();
 	if(rmode->viTVMode&VI_NON_INTERLACE) VIDEO_WaitVSync();
 
+	fb ^= 1;
+
 	// setup the fifo and then init the flipper
-	void *gp_fifo = NULL;
 	gp_fifo = memalign(32,DEFAULT_FIFO_SIZE);
 	memset(gp_fifo,0,DEFAULT_FIFO_SIZE);
  
@@ -76,6 +79,7 @@ int main( int argc, char **argv ){
 	GX_SetCopyClear(background, 0x00ffffff);
  
 	// other gx setup
+	GX_SetViewport(0,0,rmode->fbWidth,rmode->efbHeight,0,1);
 	yscale = GX_GetYScaleFactor(rmode->efbHeight,rmode->xfbHeight);
 	xfbHeight = GX_SetDispCopyYScale(yscale);
 	GX_SetScissor(0,0,rmode->fbWidth,rmode->efbHeight);
@@ -90,15 +94,12 @@ int main( int argc, char **argv ){
 		GX_SetPixelFmt(GX_PF_RGB8_Z24, GX_ZC_LINEAR);
 
 
+	GX_SetCullMode(GX_CULL_NONE);
+	GX_CopyDisp(frameBuffer[fb],GX_TRUE);
 	GX_SetDispCopyGamma(GX_GM_1_0);
- 
 
 	// setup the vertex descriptor
 	// tells the flipper to expect direct data
-	GX_ClearVtxDesc();
-	GX_SetVtxDesc(GX_VA_POS, GX_DIRECT);
-	GX_SetVtxDesc(GX_VA_TEX0, GX_DIRECT);
-
 	GX_SetVtxAttrFmt(GX_VTXFMT0, GX_VA_POS, GX_POS_XY, GX_F32, 0);
 	GX_SetVtxAttrFmt(GX_VTXFMT0, GX_VA_TEX0, GX_TEX_ST, GX_F32, 0);
 	
@@ -114,19 +115,9 @@ int main( int argc, char **argv ){
 	GX_InitTexObj(&texObj, (void*)ballsprites_bin, 64, 64, GX_TF_RGBA8, GX_CLAMP, GX_CLAMP, GX_FALSE);
 	GX_LoadTexObj(&texObj, GX_TEXMAP0);
 
-
-	guMtxIdentity(GXmodelView2D);
-	guMtxTransApply (GXmodelView2D, GXmodelView2D, 0.0F, 0.0F, -5.0F);
-	GX_LoadPosMtxImm(GXmodelView2D,GX_PNMTX0);
-
 	guOrtho(perspective,0,479,0,639,0,300);
 	GX_LoadProjectionMtx(perspective, GX_ORTHOGRAPHIC);
 
-	GX_SetViewport(0,0,rmode->fbWidth,rmode->efbHeight,0,1);
-	GX_SetBlendMode(GX_BM_BLEND, GX_BL_SRCALPHA, GX_BL_INVSRCALPHA, GX_LO_CLEAR);
-	GX_SetAlphaUpdate(GX_TRUE);
-	
-	GX_SetCullMode(GX_CULL_NONE);
 
 
 	srand(time(NULL));
@@ -151,6 +142,18 @@ int main( int argc, char **argv ){
 
 		if (WPAD_ButtonsDown(0) & WPAD_BUTTON_HOME) exit(0);
 
+		GX_SetViewport(0,0,rmode->fbWidth,rmode->efbHeight,0,1);
+		GX_InvVtxCache();
+		GX_InvalidateTexAll();
+
+		GX_ClearVtxDesc();
+		GX_SetVtxDesc(GX_VA_POS, GX_DIRECT);
+		GX_SetVtxDesc(GX_VA_TEX0, GX_DIRECT);
+
+		guMtxIdentity(GXmodelView2D);
+		guMtxTransApply (GXmodelView2D, GXmodelView2D, 0.0F, 0.0F, -5.0F);
+		GX_LoadPosMtxImm(GXmodelView2D,GX_PNMTX0);
+
 		for(i = 0; i < NUM_SPRITES; i++) {
 			sprites[i].x += sprites[i].dx;
 			sprites[i].y += sprites[i].dy;
@@ -167,17 +170,20 @@ int main( int argc, char **argv ){
 
 		GX_DrawDone();
 		
-		fb ^= 1;		// flip framebuffer
 		GX_SetZMode(GX_TRUE, GX_LEQUAL, GX_TRUE);
+		GX_SetBlendMode(GX_BM_BLEND, GX_BL_SRCALPHA, GX_BL_INVSRCALPHA, GX_LO_CLEAR);
+		GX_SetAlphaUpdate(GX_TRUE);
 		GX_SetColorUpdate(GX_TRUE);
 		GX_CopyDisp(frameBuffer[fb],GX_TRUE);
 
 		VIDEO_SetNextFramebuffer(frameBuffer[fb]);
- 
+		if(first_frame) {
+			VIDEO_SetBlack(FALSE);
+			first_frame = 0;
+		}
 		VIDEO_Flush();
- 
 		VIDEO_WaitVSync();
-
+		fb ^= 1;		// flip framebuffer
 	}
 	return 0;
 }
