7ff2446d169dd2acc5123f1f0dc4194d437b01af
Dave Murphy <davem@devkitpro.org>
2018-07-02 22:46:53 +0000

    fix includes

--- filesystem/directory/source/directory.c
+++ filesystem/directory/source/directory.c
@@ -7,7 +7,9 @@
 #include <stdlib.h>
 #include <string.h>
 #include <dirent.h>
-
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
 
 static void *xfb = NULL;
 static GXRModeObj *rmode = NULL;
