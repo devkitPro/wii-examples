e5862127d15dd72d56e6453c80dbacdf5fb1b25d
Dave Murphy <davem@devkitpro.org>
2023-04-26 16:35:03 +0100

    sockettest: use standard socket functions

--- devices/network/sockettest/source/sockettest.c
+++ devices/network/sockettest/source/sockettest.c
@@ -2,11 +2,14 @@
 #include <stdlib.h>
 #include <string.h>
 #include <malloc.h>
+#include <errno.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+
 #include <ogcsys.h>
 #include <gccore.h>
-#include <network.h>
-#include <debug.h>
-#include <errno.h>
+#include <ogc/if_config.h>
 #include <wiiuse/wpad.h>
 
 static void *xfb = NULL;
@@ -54,7 +57,7 @@ int main(int argc, char **argv) {
 		int buttonsDown = WPAD_ButtonsDown(0);
 
 		if (buttonsDown & WPAD_BUTTON_HOME) {
-			exit(0);
+			break;
 		}
 	}
 
@@ -87,11 +90,13 @@ void *httpd (void *arg) {
 
 	clientlen = sizeof(client);
 
-	sock = net_socket (AF_INET, SOCK_STREAM, IPPROTO_IP);
+	sock = socket (AF_INET, SOCK_STREAM, IPPROTO_IP);
 
-	if (sock == INVALID_SOCKET) {
-      printf ("Cannot create a socket!\n");
+	if (sock < 0) {
+		perror("socket");
     } else {
+
+    	printf("socket = %d\n", sock);
 
 		memset (&server, 0, sizeof (server));
 		memset (&client, 0, sizeof (client));
@@ -99,48 +104,47 @@ void *httpd (void *arg) {
 		server.sin_family = AF_INET;
 		server.sin_port = htons (80);
 		server.sin_addr.s_addr = INADDR_ANY;
-		ret = net_bind (sock, (struct sockaddr *) &server, sizeof (server));
+		ret = bind (sock, (struct sockaddr *) &server, sizeof (server));
 
 		if ( ret ) {
-
-			printf("Error %d binding socket!\n", ret);
-
+			perror("bind");
 		} else {
 
-			if ( (ret = net_listen( sock, 5)) ) {
-
-				printf("Error %d listening!\n", ret);
-
+			ret = listen( sock, 5);
+			if (ret) {
+				perror("listen");
 			} else {
 
 				while(1) {
 
-					csock = net_accept (sock, (struct sockaddr *) &client, &clientlen);
+					csock = accept (sock, (struct sockaddr *) &client, &clientlen);
 
 					if ( csock < 0 ) {
-						printf("Error connecting socket %d!\n", csock);
+						perror("accept");
 						while(1);
 					}
 
 					printf("Connecting port %d from %s\n", client.sin_port, inet_ntoa(client.sin_addr));
 					memset (temp, 0, 1026);
-					ret = net_recv (csock, temp, 1024, 0);
+					ret = recv (csock, temp, 1024, 0);
 					printf("Received %d bytes\n", ret);
 
 					if ( !strncmp( temp, http_get_index, strlen(http_get_index) ) ) {
 						hits++;
-						net_send(csock, http_200, strlen(http_200), 0);
-						net_send(csock, http_html_hdr, strlen(http_html_hdr), 0);
+						send(csock, http_200, strlen(http_200), 0);
+						send(csock, http_html_hdr, strlen(http_html_hdr), 0);
 						sprintf(temp, indexdata, hits);
-						net_send(csock, temp, strlen(temp), 0);
+						send(csock, temp, strlen(temp), 0);
 					}
 
-					net_close (csock);
+					close (csock);
 
 				}
 			}
 		}
 	}
+
+	close(sock);
 	return NULL;
 }
 
