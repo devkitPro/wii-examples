38a1592e3cf3c2595d052b042058bf3179ff40de
Dave Murphy <davem@devkitpro.org>
2024-04-14 23:51:10 +0100

    add eol char

--- templates/application/source/template.c
+++ templates/application/source/template.c
@@ -25,6 +25,7 @@ int main(int argc, char **argv) {
 
 	// Initialise the console, required for printf
 	console_init(xfb,20,20,rmode->fbWidth,rmode->xfbHeight,rmode->fbWidth*VI_DISPLAY_PIX_SZ);
+	//SYS_STDIO_Report(true);
 
 	// Set up the video registers with the chosen mode
 	VIDEO_Configure(rmode);
@@ -50,7 +51,7 @@ int main(int argc, char **argv) {
 	printf("\x1b[2;0H");
 
 
-	printf("Hello World!");
+	printf("Hello World!\n");
 
 	while(1) {
 
