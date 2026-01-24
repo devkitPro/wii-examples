65afeb0b286a42caeb23ee61fc7a2a1a3b39a885
Dave Murphy <davem@devkitpro.org>
2026-01-24 03:16:09 +0000

    udptest: use standard socket functions

--- devices/network/udptest/source/udptest.c
+++ devices/network/udptest/source/udptest.c
@@ -4,10 +4,16 @@
 #include <malloc.h>
 #include <ogcsys.h>
 #include <gccore.h>
-#include <network.h>
+
 #include <debug.h>
 #include <errno.h>
 #include <wiiuse/wpad.h>
+
+#include <ogc/if_config.h>
+
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <poll.h>
 
 #define NUM_SOCKETS 4
 
@@ -31,36 +37,16 @@ static void initialise() {
     if (rmode->viTVMode & VI_NON_INTERLACE) VIDEO_WaitVSync();
 }
 
-static void show_help()
-{
-    char local_ip[16] = {0};
-    const char *own_ip;
-    s32 ret;
-
-    /* Note: this call is not needed to initialize the network. We just need it
-     * to be able to show our current IP address in the message below. */
-    ret = if_config(local_ip, NULL, NULL, true, 20);
-    if (ret < 0) {
-        printf("Error retrieving own IP address.\n");
-        own_ip = "<console-ip-address>";
-    } else {
-        own_ip = local_ip;
-    }
-
-    printf("\nTo test this program, run this command from a terminal:\n"
-           "\n   nc -u %s 45678 (or 45679, ...)\n"
-           "\nthen type something and press ENTER.\n\n", own_ip);
-}
 
 static s32 create_socket(u16 port) {
     int sock;
     int ret;
     struct sockaddr_in server;
 
-    sock = net_socket(AF_INET, SOCK_DGRAM, IPPROTO_IP);
+    sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_IP);
 
-    if (sock == INVALID_SOCKET) {
-        printf ("Cannot create a socket!\n");
+    if (sock < 0) {
+        perror ("socket");
         return -1;
     }
 
@@ -69,16 +55,16 @@ static s32 create_socket(u16 port) {
     server.sin_family = AF_INET;
     server.sin_port = htons(port);
     server.sin_addr.s_addr = INADDR_ANY;
-    ret = net_bind(sock, (struct sockaddr *)&server, sizeof(server));
+    ret = bind(sock, (struct sockaddr *)&server, sizeof(server));
     if (ret) {
-        printf("Error %d binding socket!\n", ret);
+        perror("bind");
         return -1;
     }
 
     socklen_t len = sizeof(server);
-    ret = net_getsockname(sock, (struct sockaddr *)&server, &len);
+    ret = getsockname(sock, (struct sockaddr *)&server, &len);
     if (ret < 0) {
-        printf("Error %d getting socket address!\n", ret);
+        perror("getsockname");
         return -1;
     }
 
@@ -100,12 +86,12 @@ static void handle_client(s32 sock)
     s32 len, len_out;
 
     sock_len = sizeof(client);
-    /* Note: passing MSG_DONTWAIT in the flags makes net_recvfrom() return
+    /* Note: passing MSG_DONTWAIT in the flags makes recvfrom() return
      * error EOPNOTSUPP */
-    len = net_recvfrom(sock, buffer, sizeof(buffer), 0,
+    len = recvfrom(sock, buffer, sizeof(buffer), 0,
                        (struct sockaddr *)&client, &sock_len);
     if (len < 0) {
-        printf("net_recvfrom returned error %d\n", len);
+        printf("recvfrom returned error %d\n", len);
         return;
     }
     u32 addr = client.sin_addr.s_addr;
@@ -117,31 +103,48 @@ static void handle_client(s32 sock)
            client.sin_port, len, buffer);
 
     len_out = snprintf(buffer_out, sizeof(buffer_out), "Echo: %.*s\n", len, buffer);
-    net_sendto(sock, buffer_out, len_out, 0, (struct sockaddr *)&client, sock_len);
+    sendto(sock, buffer_out, len_out, 0, (struct sockaddr *)&client, sock_len);
     if (strncmp(buffer, "exit", 4) == 0) exit(0);
 }
 
 int main(int argc, char **argv) {
     s32 ret;
 
+    bool network_up = false;
+
+    char localip[16] = {0};
+    char gateway[16] = {0};
+    char netmask[16] = {0};
+
+    struct pollfd fds[NUM_SOCKETS];
+
     initialise();
 
     printf("\nlibogc network UDP demo\n");
     printf("Configuring network...\n");
 
-    net_init();
+    // Configure the network interface
+    ret = if_config ( localip, netmask, gateway, true, 20);
+    if (ret>=0) {
+        printf ("network configured, ip: %s, gw: %s, mask %s\n", localip, gateway, netmask);
 
-    struct pollsd sds[NUM_SOCKETS];
-    for (int i = 0; i < NUM_SOCKETS; i++) {
-        s32 sock = create_socket(45678 + i);
-        if (sock < 0) return EXIT_FAILURE;
+        for (int i = 0; i < NUM_SOCKETS; i++) {
+            s32 sock = create_socket(45678 + i);
+            if (sock < 0) return EXIT_FAILURE;
+            fds[i].fd = sock;
+            fds[i].events = POLLIN;
+            fds[i].revents = 0;
+        }
 
-        sds[i].socket = sock;
-        sds[i].events = POLLIN;
-        sds[i].revents = 0;
+        network_up = true;
+        printf("\nTo test this program, run this command from a terminal:\n"
+               "\n   nc -u %s 45678 (or 45679, ...)\n"
+               "\nthen type something and press ENTER.\n\n", localip);
+
+
+    } else {
+        printf ("network configuration failed!\n");
     }
-
-    show_help();
 
     while (1) {
         VIDEO_WaitVSync();
@@ -155,12 +158,14 @@ int main(int argc, char **argv) {
 
         /* This is not really needed in this case, but let's show how polling
          * works */
-        s32 timeout = 10;
-        ret = net_poll(sds, NUM_SOCKETS, timeout);
-        if (ret > 0) {
-            for (int i = 0; i < NUM_SOCKETS; i++) {
-                if (sds[i].revents) {
-                    handle_client(sds[i].socket);
+        if (network_up) {
+            s32 timeout = 10;
+            ret = poll(fds, NUM_SOCKETS, timeout);
+            if (ret > 0) {
+                for (int i = 0; i < NUM_SOCKETS; i++) {
+                    if (fds[i].revents) {
+                        handle_client(fds[i].fd);
+                    }
                 }
             }
         }
